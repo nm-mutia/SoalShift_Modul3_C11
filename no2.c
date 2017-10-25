8149e69008a05ccf94dfde035ab75ed79afc79e8
nm-mutia <nm.mutia02@gmail.com>
2017-10-25 17:44:55 +0700

    tryagain

--- no2.c
+++ no2.c
@@ -8,10 +8,11 @@
 typedef struct game{
     int lubang1[17];
     int lubang2[17];
+    int poin1;
+    int poin2;
 }hole;
 
-pthread_t tid1;
-pthread_t tid2;
+pthread_t tid[3];
 hole ho;
 
 void* playandcount(void *arg)
@@ -19,10 +20,10 @@ void* playandcount(void *arg)
     int i;
     pthread_t id = pthread_self();
 
-       	if(pthread_equal(id,tid1)){
+       	if(pthread_equal(id,tid[0])){
 	   
     	}
-	else if(pthread_equal(id,tid2){
+	else if(pthread_equal(id,tid[1]){
 	   
     	}
     
@@ -31,14 +32,47 @@ void* playandcount(void *arg)
 
 int main()
 {
-    
-
-    pthread_create(&(tid1), NULL, &playandcount, NULL);
-    pthread_create(&(tid2), NULL, &playandcount, NULL);
-    while(1){
-    	
+    int giliran, holes, lub, teb;
+    giliran = 1;
+    for(i=0; i<16; i++){
+	ho.lubang1[i] = 0;//pemain1
+	ho.lubang2[i] = 0;//pemain2
     }
-    pthread_join(tid1,NULL);
-    pthread_join(tid2,NULL);
+    
+    while(1){
+	if(giliran == 1){
+	   printf("Giliranmu! Pasang brp lubang? ");
+    	   scanf("%d", &holes);
+	   for(i=0; i<holes; i++){
+		scanf("%d",&lub);
+		ho.lubang1[lub-1] = 1;
+	   }
+	   printf("Tebak : ");
+	   for(i=0; i<holes; i++){
+	   	scanf("%d",&teb);
+	   	if(ho.lubang1[teb-1] == 1) ho.poin1++;
+		else pho.oin2++;
+	   }
+	   giliran++;
+	}
+	else{
+	   printf("Giliranmu!! Pasang brp lubang? ");
+    	   scanf("%d", &holes);
+	   for(i=0; i<holes; i++){
+		scanf("%d",&lub);
+		ho.lubang2[lub-1] = 1;
+	   }
+	   printf("Tebak : ");
+	   for(i=0; i<holes; i++){
+	   	scanf("%d",&teb);
+	   	if(ho.lubang2[teb-1] == 1) ho.poin2++;
+		else ho.poin1++;
+	   }
+	   giliran--;
+	}
+	pthread_create(&(tid[giliran-1]), NULL, &playandcount, NULL);
+    }
+    pthread_join(tid[0],NULL);
+    pthread_join(tid[1],NULL);
     return 0;
 }
