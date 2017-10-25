51bdb578763b75915f1c4e9aa70c30aefc1fec2e
nm-mutia <nm.mutia02@gmail.com>
2017-10-25 15:02:14 +0700

    update

--- soal3.c
+++ soal3.c
@@ -19,57 +19,52 @@ void* playandcount(void *arg)
     pthread_t id = pthread_self();
 
     int fak, j, k;
-    while(fis.statl > 100|| fis.statk > 100 || fis.statl <= 0 || fis.statk <= 0){	
+    while(fis.statl <= 100|| fis.statk <= 100 || fis.statl > 0 || fis.statk > 0)
+    {	
        	if(pthread_equal(id,tid[0])){
 	   sleep(10);
-	   fis.statl = fis.statl - 15;
+	   fis.statl -= 15;
 	   printf("10s Status Lohan : %d\n",fis.statl);
     	}
 	else if(pthread_equal(id,tid[1])){
 	   sleep(20);
-	   fis.statk = fis.statk - 10;
+	   fis.statk -= 10;
 	   printf("20s Status Kepiting : %d\n",fis.statk);
     	}
-    }
-    if(pthread_equal(id,tid[2])){
-    	//system("clear");
-	fis.statl = fis.statl - 15;
-	printf("Status Lohan : %d\n",fis.statl);
-    }
-    else if(pthread_equal(id,tid[3])){
-    	//system("clear");
-	fis.statk = fis.statk - 10;
-	printf("Status Kepiting : %d\n",fis.statk);
     }
     return NULL;
 }
 
 int main()
 {
+    int comm;
+    int err1 = 0, err2 = 0;
     fis.statl = 100;
     fis.statk = 100;
-    printf("Status Lohan : %d\n",fis.statl);
-    printf("Status Kepiting : %d\n",fis.statk);
-    printf("1 = Beri Makan Lohan\n");
-    printf("2 = Beri Makan Kepiting\n");
+    printf("Status Lohan 1st: %d\n",fis.statl);
+    printf("Status Kepiting 1st: %d\n",fis.statk);
+    printf("1 = Beri Makan Lohan (+10)\n");
+    printf("2 = Beri Makan Kepiting (+10)\n");
 
-    pthread_create(&(tid[0]), NULL, &playandcount, NULL);
-    pthread_create(&(tid[1]), NULL, &playandcount, NULL);
+    err1 = pthread_create(&(tid[0]), NULL, &playandcount, NULL);
+    err2 = pthread_create(&(tid[1]), NULL, &playandcount, NULL);
+    if (err1 != 0)
+    	printf("\ncan’t create thread : [%s]", strerror(err1));
+    if (err2 != 0)
+    	printf("\ncan’t create thread : [%s]", strerror(err2));
+
     while(1){
-    	int comm;
-    	int err = 0;
     	scanf("%d", &comm);
-	/*if(comm == 1){
-		//system("clear");
+	if(comm == 1){
+	   fis.statl += 10;
+	   printf("Status Lohan : %d\n",fis.statl);
 	}
 	else if(comm == 2){
-		//scanf("%s",lagu);
-	}*/
-
-	err = pthread_create(&(tid[comm+1]), NULL, &playandcount, NULL);
-    	if (err != 0){
-    		printf("\ncan’t create thread : [%s]", strerror(err));
+	   fis.statk += 10;
+	   printf("Status Kepiting : %d\n",fis.statk);
 	}
+	if(fis.statl > 100|| fis.statk > 100 || fis.statl <= 0 || fis.statk <= 0)
+	   break;
     }
     return 0;
 }
